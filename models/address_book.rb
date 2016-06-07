32b01edbffd4f5e75813433a5d6869d68a009beb
Devon Henegar <devon.henegar@gmail.com>
2016-06-07 14:24:02 -0400

    add iterative search function

--- models/address_book.rb
+++ models/address_book.rb
@@ -47,4 +47,15 @@ class AddressBook
     return nil
   end
 
+  def iterative_search(name)
+    index = 0
+    while index <= entries.length-1
+      if name == entries[index].name
+        return entries[index]
+      else index +=1
+      end
+    end
+    return nil
+  end
+
 end
